38b813d8fe148e01d9ab7fecb4a9c82071866c95
kushagra singh <kushagrasingh04897@gmail.com>
2023-07-28 21:46:28 +0530

    tough commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["Django Developer", "Web Developer", "Python Developer"],
+    default: false,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
